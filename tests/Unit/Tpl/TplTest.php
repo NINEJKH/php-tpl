1e684b7f09222bbc3a35baf1a7f8e8c90cc21e26
Gunter Grodotzki <gunter@grodotzki.co.za>
2017-11-13 19:51:41 +0200

    unit tests
    --

---
+++ tests/Unit/Tpl/TplTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace NINEJKH\Tests\Tpl;
+
+use NINEJKH\Tpl\Tpl;
+use PHPUnit\Framework\TestCase;
+
+class TplTest extends TestCase
+{
+    protected $tpl;
+
+    public function __construct()
+    {
+        $this->tpl = new Tpl(__DIR__ . '/../../Data');
+    }
+
+    public function testAddStyles()
+    {
+        $this->tpl->addStyle('http://just-a-style.com/style.css');
+
+        $this->assertContains('http://just-a-style.com/style.css', $this->tpl->getStyles());
+    }
+
+    public function testAddScripts()
+    {
+        $this->tpl->addScript('http://just-a-script.com/script.js');
+
+        $this->assertContains('http://just-a-script.com/script.js', $this->tpl->getScripts());
+    }
+}
